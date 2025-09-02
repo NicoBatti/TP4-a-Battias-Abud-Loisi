4caf94d07b4aae30e25c0fc16e59ee3a78a14e50
NicoBatti <nicolasbattias@gmail.com>
2025-09-02 14:43:14 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+import {config} from './dbconfig.js'
+import express from "express";
+import 'dotenv/config'
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+
+import pkg from 'pg'
+const {Client} = pkg;
+
+const app = express()
+app.use(express.json())
+const PORT = 8000
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
+})
+//GET a todas las tablas:
+app.get('/canciones', async (req, res) => {
+  const client = new Client(config);
+  await client.connect();
+  let result = await client.query('select * from "CANCION"');
+  await client.end();
+  console.log(result.rows);
+  res.send(result.rows)
+
+})
+
+app.post('/crearusuario', async (req, res) => {
+  const userid = req.body.userid;
+  const nombre = req.body.nombre;
+  const password = req.body.password;
+  const contraseñaHasheada = await bcrypt.hash(password, 10)
+  const client = new Client(config);
+  await client.connect();
+  let result = await client.query(`INSERT INTO "USUARIO" (id, nombre, password) VALUES ($1, $2, $3)`, [userid, nombre, contraseñaHasheada]);
+  await client.end();
+  res.send("Registro Existoso");
+
+})
+
+app.post('/login', async (req, res) => {
+    const secretKey = process.env.SECRETKEY
+    const user = req.body;
+    if (!user.userid || !user.password) {
+        return res.status(400).json({ message: "Debe completar todos los campos" });
+    }
+    try {
+        const client = new Client(config);
+        await client.connect();
+        let result = await client.query(`select * from "USUARIO" where id=$1`,[user.userid]);
+        if (result.rowCount === 0) {
+            return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+        await client.end();
+        let dbUser = result.rows[0];
+        const passOK = await bcrypt.compare(user.password, dbUser.password);
+        if (passOK) {
+            const payload = { userid: user.userid };
+            const options = { expiresIn: '1h' }; // Expirará en 1 hora. 60
+            const token = jwt.sign(payload, secretKey, options);
+            res.send({nombre: dbUser.nombre, token: token})
+        } else {
+            res.send("Clave invalida")
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/escucho', async (req, res) => {
+  const secretKey = process.env.SECRETKEY
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) {
+    return res.status(401).json({ message: "Falta token" });
+  }
+  const token = authHeader.split(" ")[1];
+  let payload;
+  try {
+    // Verifica y decodifica
+    payload = jwt.verify(token, secretKey);
+    console.log("Payload:", payload);
+
+//    res.json({ message: "Token válido", payload });
+  } catch (err) {
+    res.status(403).json({ message: "Token inválido" });
+  }
+  const client = new Client(config);
+  await client.connect();
+  let result = await client.query('select E."Reproducciones", C.nombre from "ESCUCHA" E inner join "CANCION" C on E."CancionID" = C.id WHERE E."UsuarioID" = $1', [payload.userid]);
+
+  await client.end();
+  //console.log(result.rows);
+  res.send(result.rows)
+
+})
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+})
