911138baf11694b054c7251727eab57dac0b3c74
NicoBatti <nicolasbattias@gmail.com>
2025-10-01 12:04:48 -0300

    copy paste muy poquito

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,10 +1,16 @@
 import usuariosService from "../services/usuarios.services.js"
 
 const crearUsuario = async (req,res) => {
-    //funcionalidad REST
+    //funcionalidad REST TODO: chequear lo que meti aca adentro
+    const userid = req.body.userid;
+    const nombre = req.body.nombre;
+    const password = req.body.password;
+    const rol = req.body.Rol;
+    console.log(`Rol equivale a: `, rol)
+    const contraseñaHasheada = await bcrypt.hash(password, 10)
 }
 
 const login = async (req,res) => {
     //funcionalidad REST
-    req
+    
 }
--- services/usuarios.services.js
+++ services/usuarios.services.js
@@ -3,5 +3,10 @@ import pkg from "pg";
 const {Client} = pkg;
 
 const insterUsuario = async () => {
-    //Sentencia SQl
+    //Sentencia SQl TODO: chequear lo que meti aca (mas que nada el return)
+    const client = new Client(config);
+    await client.connect();
+    let result = await client.query(`INSERT INTO "USUARIO" (id, nombre, password, "Rol") VALUES ($1, $2, $3, $4)`, [userid, nombre, contraseñaHasheada, rol]);
+    await client.end();
+    return result;
 }
