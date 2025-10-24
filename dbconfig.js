70ec2c1102b05c1a4aa02d063eb9011f8032e29b
NicoBatti <nicolasbattias@gmail.com>
2025-10-24 11:04:59 -0300

    sequelize importado correctamente

--- dbconfig.js
+++ dbconfig.js
@@ -1,8 +1,7 @@
 // export POSTGRES_HOST=ep-blue-feather-a4ve7rp2.us-east-1.aws.neon 
 // export POSTGRES_USER=default
 // export POSTGRES_PASSWORD=xxxxx
-// 
-
+import 'dotenv/config'
 
 export const config = {
     host :process.env.PGHOST,
@@ -17,3 +16,14 @@ export const config = {
         rejectUnauthorized: false
     }
 }
+
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize(process.env.DB_URL, {dialect: "postgres"});
+try{
+    await sequelize.authenticate();
+    console.log("conectado y en su prime!");
+}catch(error){
+    console.log("Mal ahi bro: ", error);
+}
+await sequelize.close();
